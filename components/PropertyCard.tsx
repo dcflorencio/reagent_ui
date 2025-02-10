2b4f5fce26f6b05ce9d4c8337ec2ab76bdfcb2ab
manishiitg <manisharies.iitg@gmail.com>
2025-02-10 19:32:45 +0530

    add request a tour and apply form

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -171,12 +171,18 @@ import "slick-carousel/slick/slick-theme.css";
 import {
     Dialog,
     DialogContent,
+    DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { loadGoogleMaps } from "@/app/utils/googleMapsLoader";
+import { CardWithForm } from "@/app/properties/components/RequestATour";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 const Heart = lazy(() => import("lucide-react").then(module => ({ default: module.Heart })))
 
 const RentalListings = ({ properties }: { properties: any[] }) => {
@@ -475,8 +481,78 @@ const RentalListings = ({ properties }: { properties: any[] }) => {
                                             </div>
 
                                             <div className="flex gap-4 mt-4">
-                                                <Button className="flex-1">Request a tour</Button>
-                                                <Button className="flex-1" variant="outline">Request to apply</Button>
+                                                <Dialog>
+                                                    <DialogTrigger asChild>
+                                                        <Button>Request a Tour</Button>
+                                                    </DialogTrigger>
+                                                    <DialogContent className="sm:max-w-[425px]">
+                                                        <DialogHeader>
+                                                            <DialogTitle>Request a Tour</DialogTitle>
+                                                            <DialogDescription>
+                                                                Make changes to your profile here. Click save when you're done.
+                                                            </DialogDescription>
+                                                        </DialogHeader>
+                                                        <form>
+                                                            <div className="grid w-full items-center gap-4">
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="name">First Name</Label>
+                                                                    <Input id="name" placeholder="First Name" />
+                                                                </div>
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="lastname">Last Name</Label>
+                                                                    <Input id="lastname" placeholder="Last Name" />
+                                                                </div>
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="email">Email</Label>
+                                                                    <Input id="email" placeholder="Email" />
+                                                                </div>
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="message">Message</Label>
+                                                                    <Textarea id="message" placeholder="Message" />
+                                                                </div>
+                                                            </div>
+                                                        </form>
+                                                        <DialogFooter>
+                                                            <Button type="submit">Request a Tour</Button>
+                                                        </DialogFooter>
+                                                    </DialogContent>
+                                                </Dialog>
+                                                <Dialog>
+                                                    <DialogTrigger asChild>
+                                                        <Button variant="secondary" className="flex-1">Request to apply</Button>
+                                                    </DialogTrigger>
+                                                    <DialogContent className="sm:max-w-[425px]">
+                                                        <DialogHeader>
+                                                            <DialogTitle>Request to apply</DialogTitle>
+                                                            <DialogDescription>
+                                                                Make changes to your profile here. Click save when you're done.
+                                                            </DialogDescription>
+                                                        </DialogHeader>
+                                                        <form>
+                                                            <div className="grid w-full items-center gap-4">
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="name">First Name</Label>
+                                                                    <Input id="name" placeholder="First Name" />
+                                                                </div>
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="lastname">Last Name</Label>
+                                                                    <Input id="lastname" placeholder="Last Name" />
+                                                                </div>
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="email">Email</Label>
+                                                                    <Input id="email" placeholder="Email" />
+                                                                </div>
+                                                                <div className="flex flex-col space-y-1.5">
+                                                                    <Label htmlFor="message">Message</Label>
+                                                                    <Textarea id="message" placeholder="Message" />
+                                                                </div>
+                                                            </div>
+                                                        </form>
+                                                        <DialogFooter>
+                                                            <Button type="submit">Request to apply</Button>
+                                                        </DialogFooter>
+                                                    </DialogContent>
+                                                </Dialog>
                                             </div>
                                         </div>
                                     </div>
