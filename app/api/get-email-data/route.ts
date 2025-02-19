52a55473427804c47b50326bc9cf6e199b3ecfca
manishiitg <manisharies.iitg@gmail.com>
2025-02-19 17:47:09 +0530

    local

--- app/api/get-email-data/route.ts
+++ app/api/get-email-data/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 export async function POST(req: NextRequest): Promise<NextResponse<any>> {
+    console.log("get-email-data api called");
     if (req.method !== 'POST') {
         return NextResponse.json({ success: false, status: 405, data: { error: 'Method not allowed' } });
     }
+
     const data = await req.json();
     console.log("data", data);
     return NextResponse.json({ success: true, status: 200, data: data });
